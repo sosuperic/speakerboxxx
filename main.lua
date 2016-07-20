d9578b10b3ece58faad736ce0cdb4d5164b4e144
Eric Chu <eric.chu.56@gmail.com>
2016-07-20 03:58:21 -0400

    [Networks] Miscellaneous - test on valid, GPU, saving, etc.
    Additions
    - GPU works (but no CUDNN - need to upgrade from 5004 to 5005)
    - Save things: create save directory using current time, save
      model at end of epochs (flag to determine how often), losses
      and average run time to text file using optim logger, command
      line arguments to .t7 and csv file
    - More command line arguments. Notable: -dont_save
    - Calculate validation loss at end of every epoch. There seems to be
      2 approaches to do so.
      1) Have if clauses checking self.training in the hooks (e.g. the
         on.ForwardCriterion hook will trigger for both engine:train()
         and engine:test()). I think the mnist example does this.
      2) Have a second engine, with separate hooks for each.
      I chose option 2.
    
    Other note:
    - Training loss printed is cumulative for that epoch. It then resets
      at start of next epoch
    
    Test plan:
    On Shannon
    - th main.lua -model duration -gpuid 0 -method adam
    - th main.lua -model acoustic -gpuid 0
    Locally
    - th main.lua -model duration
    - th main.lua -model acoustic

--- main.lua
+++ main.lua
@@ -6,31 +6,29 @@
 cmd = torch.CmdLine()
 cmd:text()
 cmd:text('Example:')
-cmd:text('$ th g2p.lua -use_google_model -lr 0.001 -lr_decay 1e-9 -mom 0.9 -damp 0.9 -nesterov true')
-cmd:text('$ th g2p.lua -use_google_model -load_model -load_model_dir 2016_6_23___12_14_32 -load_model_fn e4_2.7609.t7')
+cmd:text('$ th main.lua -model duration -save_model_every_epoch 5')
 cmd:text('Options:')
 -- Training
 cmd:option('-model', '', 'duration or acoustic')
 cmd:option('-train_on_valid', false, 'Train on valid instead of training. Used to debug because it is faster')
 cmd:option('-batchsize', 32, 'number of examples in minibatch')
-cmd:option('-epochs', 100, 'max number of epochs to train for')
+cmd:option('-maxepochs', 100, 'max number of epochs to train for')
 -- Load model
-cmd:option('-load_model', false, 'start training from existing model')
-cmd:option('-load_model_dir', '', 'directory to load model and params from')
-cmd:option('-load_model_fn', '', 'fn of model to load')
+-- cmd:option('-load_model', false, 'start training from existing model')
+-- cmd:option('-load_model_dir', '', 'directory to load model and params from')
+-- cmd:option('-load_model_fn', '', 'fn of model to load')
 -- Optimization
-cmd:option('-use_rmsprop', false, 'Use RMSprop to optimize')
-cmd:option('-use_adam', false, 'Use Adam to optimize')
-cmd:option('-lr', 0.1, 'learning rate')
+cmd:option('-method','sgd', 'which optimization method to use')
+cmd:option('-lr', 1e-3, 'learning rate')
 cmd:option('-lr_decay', 0, 'learning rate decay')
-cmd:option('-weight_decay', 0, 'weight decay')
 cmd:option('-mom', 0, 'momentum')
 cmd:option('-damp', 0, 'dampening')
 cmd:option('-nesterov', false, 'Nesterov momentum')
 -- Bookkeeping
 cmd:option('-models_dir', 'models', 'directory to save models to')
 cmd:option('-gpuid', -1, 'ID of gpu to run on')
-cmd:option('-save_model_every_epoch', 1, 'how often to save model')
+cmd:option('-dont_save', false, 'Save or not. Use true for testing / debugging')
+cmd:option('-save_model_every_epoch', 5, 'how often to save model')
 cmd:option('-notes', '', 'String of notes, e.g. using batch norm. To keep track of iterative testing / small modifications')
 local opt = cmd:parse(arg)
 
@@ -39,5 +37,5 @@ local opt = cmd:parse(arg)
 ---------------------------------------------------------------------------
 local network = require 'Network'
 network:init(opt)
-network:train()
+network:train(opt)
 
--- network.lua
+++ network.lua
@@ -1,32 +1,45 @@
 require 'optim'
-require 'rnn'
-require 'nn'
+require 'socket'
+require 'pl'
+require 'csvigo'
+require 'utils.lua_utils'
 
 local Network = {}
--- local Network = torch.class('Network')
 
 function Network:init(opt)
     self.tnt = require 'torchnet'
     self:setup_model(opt)
     self:setup_engine(opt)
     self:setup_gpu(opt)
-    self:make_save_directory(opt)    -- TODO: move this to training so we don't make directories for errrorss
 end
 
 
 function Network:setup_model(opt)
+    local method = {
+        sgd = optim.sgd,
+        adam = optim.adam,
+        adagrad = optim.adagrad,
+        adadelta = optim.adadelta,
+        rmsprop = optim.rmsprop
+    }
+    self.optim_method = method[opt.method]
+
+    local split = ternary_op(opt.train_on_valid, 'valid', 'train')
+
     if opt.model == 'duration' then
         local net, criterion = unpack(require 'duration_model')
         self.net = net
         self.criterion = criterion
         self.model = 'duration'
-        self.iterator = self:get_iterator('duration', 'train')
+        self.train_iterator = self:get_iterator(opt.batchsize, 'duration', split)
+        self.valid_iterator = self:get_iterator(opt.batchsize, 'duration', 'valid')
     elseif opt.model == 'acoustic' then
         local net, criterion = unpack(require 'acoustic_model')
         self.net = net
         self.criterion = criterion
         self.model = 'acoustic'
-        self.iterator = self:get_iterator('acoustic', 'train')
+        self.train_iterator = self:get_iterator(opt.batchsize, 'acoustic', split)
+        self.valid_iterator = self:get_iterator(opt.batchsize, 'acoustic', 'valid')
     else
         print('model must be duration or acoustic')
         os.exit()
@@ -34,66 +47,127 @@ function Network:setup_model(opt)
 end
 
 function Network:setup_engine(opt)
-    -- Set up engine and define hooks
+    -- Set up engines and define hooks
     self.engine = self.tnt.OptimEngine()
-    -- local val_engine = 
-    local train_meter  = self.tnt.AverageValueMeter()
-    local val_meter = self.tnt.AverageValueMeter()
-    local timer = self.tnt.TimeMeter{unit=true}
-    self.engine.hooks.onStart = function(state)
-        -- state.epoch
-        -- state.maxepoch
-        -- state.config
-    end
+    self.train_meter  = self.tnt.AverageValueMeter()
+    self.valid_engine = self.tnt.SGDEngine()
+    self.valid_meter = self.tnt.AverageValueMeter()
+    self.timer = self.tnt.TimeMeter{unit=true}
+
+    -- Hooks for main (training) engine
     self.engine.hooks.onStartEpoch = function(state)
-        train_meter:reset()
+        self.train_meter:reset()
     end
     self.engine.hooks.onForwardCriterion = function(state)
-        train_meter:add(state.criterion.output)
-        if state.training then
-            print(string.format('Epoch: %d; avg. loss: %2.4f',
-                state.epoch, train_meter:value()))
-        end
+        self.train_meter:add(state.criterion.output)
+        print(string.format('Epoch: %d; avg. loss: %2.4f',
+            state.epoch, self.train_meter:value()))
     end
     self.engine.hooks.onEndEpoch = function(state)
-        timer:incUnit()
-        print(string.format('Avg time for one epoch: %.4f',
-            timer:value()))
+        -- Create directory to save models, etc. at end of first epoch
+        -- Do it now to minimize chance of error and useless folder being created 
+        if state.epoch == 1 and (not opt.dont_save) then
+            self:make_save_directory(opt)
+            self:save_opt(opt)
+            self:setup_logger(opt)
+        end
 
-        -- measure test loss and error:
-        -- val_meter:reset()
-        -- engine:test{
-        --    network   = net,
-        --    iterator  = getIterator('valid'),
-        --    criterion = criterion,
-        -- }
-        -- print(state.network.output)
-        -- print(string.format('Epoch: %d; test loss: %2.4f',
-        --    val_meter:value()))
+        -- Get loss on validation
+        print('Getting validation loss')
+        self.valid_engine:test{
+            network   = self.net,
+            iterator  = self.valid_iterator,
+            criterion = self.criterion
+        }
+        local train_loss = self.train_meter:value()
+        local valid_loss = self.valid_meter:value()
+        if not opt.dont_save then
+            self.logger:add{train_loss, valid_loss, self.timer:value()}
+        end
+
+        -- Timer
+        self.timer:incUnit()
+        print(string.format('Avg time for one epoch: %.4f',
+            self.timer:value()))
+
+        -- Save model and loss
+        if (state.epoch % opt.save_model_every_epoch == 0) and (not opt.dont_save) then
+            local fn = string.format('net_e%d.t7', state.epoch)
+            self:save_network(fn)
+        end
     end
     self.engine.hooks.onEnd = function(state)
-        -- torch.save('net.t7', net)
+        if not opt.dont_save then
+            local fn = string.format('net_e%d.t7', state.epoch)
+            self:save_network(fn)
+        end
     end
+
+    -- Hooks for validation engine
+    self.valid_engine.hooks.onStartEpoch = function(state)
+        self.valid_meter:reset()
+    end
+    self.valid_engine.hooks.onForwardCriterion = function(state)
+        self.valid_meter:add(state.criterion.output)
+    end
+    self.valid_engine.hooks.onEnd = function(state)
+        print(string.format('Validation avg. loss: %2.4f',
+            self.valid_meter:value()))
+    end
+end
+
+function Network:make_save_directory(opt)
+    -- Create directory (if necessary) to save models to using current time 
+    local cur_dt = os.date('*t', socket.gettime())
+    local save_dirname = string.format('%d_%d_%d___%d_%d_%d',
+        cur_dt.year, cur_dt.month, cur_dt.day,
+        cur_dt.hour, cur_dt.min, cur_dt.sec)
+    save_path = path.join(opt.models_dir, self.model, save_dirname)
+    make_dir_if_not_exists(save_path)
+    self.save_path = save_path
+end
+
+function Network:save_opt(opt)
+    local fp = path.join(self.save_path, 'cmd')
+    torch.save(fp .. '.t7', opt)
+    csvigo.save{path=fp .. '.csv', data=convert_table_for_csvigo(opt)}
+end
+
+function Network:save_network(fn)
+    local fp = path.join(self.save_path, fn)
+    print(string.format('Saving model to: %s', fp))
+    torch.save(fp, self.net)
+end
+
+
+function Network:setup_logger(opt)
+    local fp = path.join(self.save_path, 'stats.log')
+    self.logger = optim.Logger(fp)
+    self.logger:setNames{'Train loss', 'Valid loss', 'Avg. epoch time'}
 end
 
 function Network:setup_gpu(opt)
     if opt.gpuid >= 0 then
-        local id = (3 - opt.gpuid) + 1
-        print(string.format('Using GPU %d', id))
-
         require 'cunn'
         require 'cutorch'
-        require 'cudnn'
-        cutorch.setDevice(id)
+        -- require 'cudnn'
+        print(string.format('Using GPU %d', opt.gpuid))
+        cutorch.setDevice((3 - opt.gpuid) + 1)
         cutorch.manualSeed(123)
 
-        self.net = net:cuda()
-        self.criterion = criterion:cuda()
+        self.net = self.net:cuda()
+        self.criterion = self.criterion:cuda()
 
         -- Copy sample to GPU buffer
         -- alternatively, this logic can be implemented via a TransformDataset
         local igpu, tgpu = torch.CudaTensor(), torch.CudaTensor()
-        engine.hooks.onSample = function(state)
+        self.engine.hooks.onSample = function(state)
+            igpu:resize(state.sample.input:size() ):copy(state.sample.input)
+            tgpu:resize(state.sample.target:size()):copy(state.sample.target)
+            state.sample.input  = igpu
+            state.sample.target = tgpu
+        end
+        self.valid_engine.hooks.onSample = function(state)
             igpu:resize(state.sample.input:size() ):copy(state.sample.input)
             tgpu:resize(state.sample.target:size()):copy(state.sample.target)
             state.sample.input  = igpu
@@ -102,10 +176,7 @@ function Network:setup_gpu(opt)
     end
 end
 
-function Network:make_save_directory(opt)
-end
-
-function Network:get_iterator(model, split)
+function Network:get_iterator(batchsize, model, split)
     return self.tnt.ParallelDatasetIterator{
         nthread = 1,
         closure = function()
@@ -120,9 +191,9 @@ function Network:get_iterator(model, split)
             elseif model == 'acoustic' then
                 dataset = tnt.AcousticDataset(split)
             end
-            
+
             return tnt.BatchDataset{
-                batchsize = 32,
+                batchsize = batchsize,
                 dataset = dataset,
             }
         end,
@@ -146,17 +217,20 @@ function Network:get_iterator(model, split)
     }
 end
 
-function Network:train()
+function Network:train(opt)
     self.engine:train{
         network   = self.net,
-        iterator  = self:get_iterator(self.model, 'train'),
+        iterator  = self.train_iterator,
         criterion = self.criterion,
-        optimMethod = optim.sgd,
+        optimMethod = self.optim_method,
         config = {
-            learningRate = 1e-2,
-            momentum = 0.9,
+            learningRate = opt.lr,
+            learningRateDecay = opt.lr_decay,
+            momentum = opt.mom,
+            dampening = opt.damp,
+            nesterov = opt.nesterov,
         },
-        maxepoch  = 100,
+        maxepoch  = opt.maxepochs,
     }
 end
 
